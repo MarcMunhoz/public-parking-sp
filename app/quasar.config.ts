e17f5e2cb7a48ded961e1641ffa445f305f28743
Marcelo Munhoz <me@marcelomunhoz.com>
2026-04-22 13:33:06 -0300

    style(css): Separates Tailwind imports from app styles
    - Add `tailwind.scss` and register it in Quasar CSS config
    - Remove Tailwind directives from `app.scss`
    - Replace `@apply` in `.glass-panel` with equivalent plain CSS

--- app/quasar.config.ts
+++ app/quasar.config.ts
@@ -19,7 +19,8 @@ export default defineConfig((/* ctx */) => {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: [
-      'app.scss'
+      'app.scss',
+      'tailwind.scss'
     ],
 
     // https://github.com/quasarframework/quasar/tree/dev/extras
